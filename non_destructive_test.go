c5ec25f40b3044d92bfbd21796ee5f1ac5ce6cff
Andreas Fuchs <asf@boinkor.net>
2019-03-16 12:09:09 +0100

    Add a test for the first 256 possible ring buffers' indexes
    This ensures that All() and Rev() return indexes matching those of
    Start1/End1/End2.

--- non_destructive_test.go
+++ non_destructive_test.go
@@ -1,12 +1,14 @@
 package o
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestAll(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
 		name     string
 		ra       Ring
@@ -32,6 +34,7 @@ func TestAll(t *testing.T) {
 }
 
 func TestRev(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
 		name     string
 		ra       Ring
@@ -57,6 +60,7 @@ func TestRev(t *testing.T) {
 }
 
 func TestStartEnd(t *testing.T) {
+	t.Parallel()
 	tests := []struct {
 		name               string
 		ra                 Ring
@@ -86,6 +90,46 @@ func TestStartEnd(t *testing.T) {
 			assert.Equal(t, test.start1, Start1(test.ra))
 			assert.Equal(t, test.end1, End1(test.ra))
 			assert.Equal(t, test.end2, End2(test.ra))
+		})
+	}
+}
+
+func TestMatching(t *testing.T) {
+	var toinsert = 263 // prime & greater than the max. capacity
+	for i := uint(1); i < 256; i++ {
+		test := i
+		t.Run(fmt.Sprintf("%03d", test), func(t *testing.T) {
+			t.Parallel()
+			ra := NewRing(test)
+			for i := 0; i < toinsert; i++ {
+				ForcePush(ra)
+			}
+			assert.Equal(t, ra.Size(), test)
+
+			t.Log(Start1(ra), End1(ra), End2(ra))
+
+			all := All(ra)
+			t.Log("Checking All()=", all)
+			assert.Equal(t, uint(len(all)), test)
+			assert.Equal(t, all[0], Start1(ra),
+				"start index should be the same for All() and Start1()")
+			assert.Equal(t, uint(len(all)), End1(ra),
+				"End1() should always stop at the end of the array")
+			assert.Equal(t, Start1(ra), End2(ra),
+				"End2() should always return the index of Start1()")
+			assert.Equal(t, (all[len(all)-1]+1)%uint(len(all)), End2(ra),
+				"End2() should be one past the last index of All()")
+
+			rev := Rev(ra)
+			// The loop for going in reverse looks like:
+			//     for i := ring.Mask(o.Start1(ring)-1); i > 0; i-- {}
+			//     for i := o.End1(ring)-1; i >= o.Start1(ring); i-- {}
+			t.Log("Checking Rev()=", rev)
+			assert.Equal(t, uint(len(rev)), test)
+			assert.Equal(t, rev[0], ra.Mask(Start1(ra)-1),
+				"start index should be the same for Rev() and the one before Start1()")
+			assert.Equal(t, rev[len(rev)-1], Start1(ra),
+				"The last entry in Rev() should be Start1()")
 		})
 	}
 }
