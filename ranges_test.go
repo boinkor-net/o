f5a73076e251b139daf2c93fe70166e563e88eca
Andreas Fuchs <asf@boinkor.net>
2019-03-17 22:50:27 +0100

    Add index traversal sanity proptest
    This is a better version of the hardcoded subtests in ranges_test.go

--- ranges_test.go
+++ ranges_test.go
@@ -1,12 +1,10 @@
 package o_test
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/antifuchs/o"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
 func TestLIFO(t *testing.T) {
@@ -67,51 +65,6 @@ func TestFIFO(t *testing.T) {
 				results = append(results, s.Value())
 			}
 			assert.Equal(t, test.expected, results)
-		})
-	}
-}
-
-func TestMatching(t *testing.T) {
-	var toinsert = 263 // prime & greater than the max. capacity
-	const min uint = 1
-	const max uint = 256
-	for n := min; n <= max; n++ {
-		test := n
-		t.Run(fmt.Sprintf("%03d", test), func(t *testing.T) {
-			t.Parallel()
-			ra := o.NewRing(test)
-			for i := 0; i < toinsert; i++ {
-				o.ForcePush(ra)
-			}
-			assert.Equal(t, ra.Size(), test)
-
-			fifo := make([]uint, 0, test)
-			lifo := make([]uint, 0, test)
-
-			s := o.ScanLIFO(ra)
-			for i := 0; s.Next(); i++ {
-				lifo = append(lifo, s.Value())
-			}
-
-			s = o.ScanFIFO(ra)
-			for i := 0; s.Next(); i++ {
-				fifo = append(fifo, s.Value())
-			}
-			assert.Equal(t, len(lifo), len(fifo))
-			assert.Equal(t, fifo[0], lifo[len(lifo)-1])
-			assert.Equal(t, lifo[0], fifo[len(fifo)-1])
-			// check contiguity:
-			last := lifo[0]
-			for nth, i := range lifo[1:] {
-				require.Equal(t, ra.Mask(last+1), i, "at %d", nth)
-				last = i
-			}
-
-			last = fifo[0]
-			for nth, i := range fifo[1:] {
-				require.Equal(t, ra.Mask(last+test-1), i, "at %d", nth)
-				last = i
-			}
 		})
 	}
 }
