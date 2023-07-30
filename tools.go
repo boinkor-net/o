74314e852349537c1295c177f0cab2ec474b4d84
Andreas Fuchs <asf@boinkor.net>
2023-07-30 14:18:01 -0400

    Use golangci-lint action instead of hand-coding it in modules

--- tools.go
+++ tools.go
@@ -1,9 +1,9 @@
 // +build tools
+//go:build tools
 
 package tools
 
 import (
 	_ "github.com/cespare/prettybench"
-	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/wadey/gocovmerge"
 )
